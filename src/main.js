ea3239706a345d904268182ceaabd1a985cbd1e5
Garmhausen <robertgarmhausen@gmail.com>
2021-02-26 17:59:49 -0500

    remove vuex, not using

--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
 import Vue from 'vue'
-import Vuex from 'vuex';
 import App from './App.vue'
 
 import './assets/styles/index.css'
 import router from './router'
 
-Vue.use(Vuex);
-
 Vue.config.productionTip = false
-
-// const store = new Vuex.Store({
-//   state: {
-//     something: true,
-//   },
-//   mutations: {
-//     switch(state) {
-//       state.something = !state.someting;
-//     }
-//   }
-// })
 
 new Vue({
   router,
